a219c10a7302b8f82f11164d1001cd5dcabc1115
Usuario <Usuario@JOHNDAY>
2020-06-01 11:03:39 -0500

    Tarea 1

--- WindowsFormsApp2/Formlista.Designer.cs
+++ WindowsFormsApp2/Formlista.Designer.cs
@@ -28,12 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.btnagregar = new System.Windows.Forms.Button();
+            this.lstNombres = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese su nombre";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(206, 62);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(120, 22);
+            this.txtnombre.TabIndex = 1;
+            this.txtnombre.TextChanged += new System.EventHandler(this.text_TextChanged);
+            this.txtnombre.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtnombre_KeyDown);
+            this.txtnombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtnombre_KeyPress);
+            // 
+            // btnagregar
+            // 
+            this.btnagregar.Location = new System.Drawing.Point(350, 62);
+            this.btnagregar.Name = "btnagregar";
+            this.btnagregar.Size = new System.Drawing.Size(94, 30);
+            this.btnagregar.TabIndex = 2;
+            this.btnagregar.Text = "Agregar";
+            this.btnagregar.UseVisualStyleBackColor = true;
+            this.btnagregar.Click += new System.EventHandler(this.btnagregar_Click);
+            this.btnagregar.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.btnagregar_KeyPress);
+            // 
+            // lstNombres
+            // 
+            this.lstNombres.FormattingEnabled = true;
+            this.lstNombres.ItemHeight = 16;
+            this.lstNombres.Location = new System.Drawing.Point(47, 146);
+            this.lstNombres.Name = "lstNombres";
+            this.lstNombres.Size = new System.Drawing.Size(314, 196);
+            this.lstNombres.TabIndex = 3;
+            // 
+            // Formlista
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstNombres);
+            this.Controls.Add(this.btnagregar);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.label1);
+            this.Name = "Formlista";
             this.Text = "Formlista";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.Button btnagregar;
+        private System.Windows.Forms.ListBox lstNombres;
     }
 }
--- WindowsFormsApp2/Formlista.cs
+++ WindowsFormsApp2/Formlista.cs
@@ -16,5 +16,55 @@ namespace WindowsFormsApp2
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnagregar_Click(object sender, EventArgs e)
+        {
+            string nombre;
+            string pulso = " ";
+            nombre = this.txtnombre.Text;
+                pulso += nombre + System.Environment.NewLine;
+            this.lstNombres.Items.Add(pulso);
+        }
+
+        private void text_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnagregar_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           
+        }
+
+        private void txtnombre_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+
+                string nombre;
+            string pulso = " ";
+            nombre = this.txtnombre.Text;
+                pulso += nombre + System.Environment.NewLine;
+            this.lstNombres.Items.Add(pulso);
+            }
+
+        }
+
+        private void txtnombre_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.Equals(Keys.Enter))
+            {
+                if (this.txtnombre.Text.Length > 0)
+                {
+                    this.lstNombres.Items.Add(this.txtnombre.Text);
+                    this.txtnombre.Text = "";
+                }
+            }
+        }
     }
 }
