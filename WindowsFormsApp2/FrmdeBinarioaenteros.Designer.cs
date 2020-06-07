938f0e612c2539c827cd97f6fb3329b50cbb73b4
Usuario <Usuario@JOHNDAY>
2020-06-07 17:39:52 -0500

    Tarea de Binarios a enteros

--- WindowsFormsApp2/FrmdeBinarioaenteros.Designer.cs
+++ WindowsFormsApp2/FrmdeBinarioaenteros.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp2
 {
-    partial class FrmNumerosEnteros
+    partial class FrmdeBinarioaenteros
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.btbcalcular = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.checkBox3 = new System.Windows.Forms.CheckBox();
             this.checkBox4 = new System.Windows.Forms.CheckBox();
@@ -38,12 +37,26 @@
             this.checkBox6 = new System.Windows.Forms.CheckBox();
             this.checkBox7 = new System.Windows.Forms.CheckBox();
             this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.checkBox10 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 67);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(18, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.checkBox10);
+            this.groupBox1.Controls.Add(this.checkBox9);
             this.groupBox1.Controls.Add(this.checkBox8);
             this.groupBox1.Controls.Add(this.checkBox7);
             this.groupBox1.Controls.Add(this.checkBox6);
@@ -52,48 +65,26 @@
             this.groupBox1.Controls.Add(this.checkBox3);
             this.groupBox1.Controls.Add(this.checkBox2);
             this.groupBox1.Controls.Add(this.checkBox1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 44);
+            this.groupBox1.Location = new System.Drawing.Point(12, 36);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(560, 144);
-            this.groupBox1.TabIndex = 0;
+            this.groupBox1.Size = new System.Drawing.Size(512, 146);
+            this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Equivalente Binario";
-            // 
-            // btbcalcular
-            // 
-            this.btbcalcular.Location = new System.Drawing.Point(619, 98);
-            this.btbcalcular.Name = "btbcalcular";
-            this.btbcalcular.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.btbcalcular.Size = new System.Drawing.Size(122, 62);
-            this.btbcalcular.TabIndex = 1;
-            this.btbcalcular.Text = "Calcular ";
-            this.btbcalcular.UseVisualStyleBackColor = true;
-            this.btbcalcular.Click += new System.EventHandler(this.btbcalcular_Click);
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(6, 54);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(18, 17);
-            this.checkBox1.TabIndex = 0;
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            this.groupBox1.Text = "Binario";
             // 
             // checkBox2
             // 
             this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(78, 54);
+            this.checkBox2.Location = new System.Drawing.Point(55, 67);
             this.checkBox2.Name = "checkBox2";
             this.checkBox2.Size = new System.Drawing.Size(18, 17);
             this.checkBox2.TabIndex = 1;
             this.checkBox2.UseVisualStyleBackColor = true;
-            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
             // 
             // checkBox3
             // 
             this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(143, 54);
+            this.checkBox3.Location = new System.Drawing.Point(104, 67);
             this.checkBox3.Name = "checkBox3";
             this.checkBox3.Size = new System.Drawing.Size(18, 17);
             this.checkBox3.TabIndex = 2;
@@ -102,7 +93,7 @@
             // checkBox4
             // 
             this.checkBox4.AutoSize = true;
-            this.checkBox4.Location = new System.Drawing.Point(211, 54);
+            this.checkBox4.Location = new System.Drawing.Point(154, 67);
             this.checkBox4.Name = "checkBox4";
             this.checkBox4.Size = new System.Drawing.Size(18, 17);
             this.checkBox4.TabIndex = 3;
@@ -111,7 +102,7 @@
             // checkBox5
             // 
             this.checkBox5.AutoSize = true;
-            this.checkBox5.Location = new System.Drawing.Point(287, 54);
+            this.checkBox5.Location = new System.Drawing.Point(207, 67);
             this.checkBox5.Name = "checkBox5";
             this.checkBox5.Size = new System.Drawing.Size(18, 17);
             this.checkBox5.TabIndex = 4;
@@ -120,7 +111,7 @@
             // checkBox6
             // 
             this.checkBox6.AutoSize = true;
-            this.checkBox6.Location = new System.Drawing.Point(366, 54);
+            this.checkBox6.Location = new System.Drawing.Point(265, 67);
             this.checkBox6.Name = "checkBox6";
             this.checkBox6.Size = new System.Drawing.Size(18, 17);
             this.checkBox6.TabIndex = 5;
@@ -129,7 +120,7 @@
             // checkBox7
             // 
             this.checkBox7.AutoSize = true;
-            this.checkBox7.Location = new System.Drawing.Point(440, 54);
+            this.checkBox7.Location = new System.Drawing.Point(326, 67);
             this.checkBox7.Name = "checkBox7";
             this.checkBox7.Size = new System.Drawing.Size(18, 17);
             this.checkBox7.TabIndex = 6;
@@ -138,29 +129,57 @@
             // checkBox8
             // 
             this.checkBox8.AutoSize = true;
-            this.checkBox8.Location = new System.Drawing.Point(519, 54);
+            this.checkBox8.Location = new System.Drawing.Point(384, 67);
             this.checkBox8.Name = "checkBox8";
             this.checkBox8.Size = new System.Drawing.Size(18, 17);
             this.checkBox8.TabIndex = 7;
             this.checkBox8.UseVisualStyleBackColor = true;
             // 
+            // checkBox9
+            // 
+            this.checkBox9.AutoSize = true;
+            this.checkBox9.Location = new System.Drawing.Point(439, 67);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Size = new System.Drawing.Size(18, 17);
+            this.checkBox9.TabIndex = 8;
+            this.checkBox9.UseVisualStyleBackColor = true;
+            // 
+            // checkBox10
+            // 
+            this.checkBox10.AutoSize = true;
+            this.checkBox10.Location = new System.Drawing.Point(488, 67);
+            this.checkBox10.Name = "checkBox10";
+            this.checkBox10.Size = new System.Drawing.Size(18, 17);
+            this.checkBox10.TabIndex = 9;
+            this.checkBox10.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(579, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 49);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Convertir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(619, 187);
+            this.textBox1.Location = new System.Drawing.Point(579, 176);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(134, 22);
-            this.textBox1.TabIndex = 2;
+            this.textBox1.Size = new System.Drawing.Size(124, 22);
+            this.textBox1.TabIndex = 3;
             // 
-            // FrmNumerosEnteros
+            // FrmdeBinarioaenteros
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.btbcalcular);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
-            this.Name = "FrmNumerosEnteros";
-            this.Text = "FrmNumerosEnteros";
+            this.Name = "FrmdeBinarioaenteros";
+            this.Text = "FrmdeBinarioaenteros";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
@@ -170,16 +189,18 @@
 
         #endregion
 
+        private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox10;
+        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox6;
         private System.Windows.Forms.CheckBox checkBox5;
         private System.Windows.Forms.CheckBox checkBox4;
         private System.Windows.Forms.CheckBox checkBox3;
         private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.Button btbcalcular;
-        private System.Windows.Forms.CheckBox checkBox8;
-        private System.Windows.Forms.CheckBox checkBox7;
-        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
     }
 }
---
+++ WindowsFormsApp2/FrmdeBinarioaenteros.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class FrmdeBinarioaenteros : Form
+    {
+        public FrmdeBinarioaenteros()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int cantidad = 0;
+            
+            string modus = "";
+            if (checkBox1.Checked == true)
+            {
+                int  k=2,equivalencia=0;
+                equivalencia =(int)Math.Pow(k, 9);
+                cantidad += equivalencia;
+
+            }
+            if (checkBox2.Checked == true)
+            {
+                int k = 2, equivalencia = 0;
+                equivalencia = (int)Math.Pow(k, 8);
+                cantidad += equivalencia;
+
+            }
+            if (checkBox3.Checked == true)
+            {
+                int k = 2, equivalencia = 0;
+                equivalencia = (int)Math.Pow(k, 7);
+                cantidad += equivalencia;
+
+            }
+            if (checkBox4.Checked == true)
+            {
+                int k = 2, equivalencia = 0;
+                equivalencia = (int)Math.Pow(k, 6);
+                cantidad += equivalencia;
+
+            }
+            if (checkBox5.Checked == true)
+            {
+                int k = 2, equivalencia = 0;
+                equivalencia = (int)Math.Pow(k,5);
+                cantidad += equivalencia;
+
+            }
+            if (checkBox6.Checked == true)
+            {
+                int k = 2, equivalencia = 0;
+                equivalencia = (int)Math.Pow(k, 4);
+                cantidad += equivalencia;
+
+            }
+            if (checkBox7.Checked == true)
+            {
+                int k = 2, equivalencia = 0;
+                equivalencia = (int)Math.Pow(k, 3);
+                cantidad += equivalencia;
+
+            }
+            if (checkBox8.Checked == true)
+            {
+                int k = 2, equivalencia = 0;
+                equivalencia = (int)Math.Pow(k, 2);
+                cantidad += equivalencia;
+
+            }
+            if (checkBox9.Checked == true)
+            {
+                int k = 2, equivalencia = 0;
+                equivalencia = (int)Math.Pow(k, 1);
+                cantidad += equivalencia;
+
+            }
+            if (checkBox10.Checked == true )
+            {
+                int k = 2, equivalencia = 0;
+                equivalencia = (int)Math.Pow(k, 0);
+                cantidad += equivalencia;
+
+            }
+
+            modus = cantidad.ToString();
+            textBox1.Text = modus;
+           
+        }
+      
+        
+    }
+}
